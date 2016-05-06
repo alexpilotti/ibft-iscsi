4521e3bb301cc855b81c0d24a20d349610355365
Sergiu Strat <Sergiu Strat>
2016-05-06 15:50:56 +0300

    iscsifix + suffix option for multiple runs

---
+++ ssiscsi/iscsifix.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem Cloudbase Solution Labs
+rem System Dump
+rem v1.0 / 20160331
+rem Sergiu Strat <sstrat@cloudbasesolutions.com>
+
+if [%1]==[] goto usage
+if [%2]==[] goto usage
+
+set windir=%1
+set outdir=%2
+set suffix=%3
+
+dir /b > %outdir%\iscsi-fix%suffix%.log
+type %outdir%\iscsi-fix%suffix%.log
+
+goto end
+
+:usage
+echo Usage: %0 winDir outDir [suffix]
+exit /b 1
+
+:end
